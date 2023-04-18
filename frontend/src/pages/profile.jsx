81d13f64964309097087002471d7882872c74e75
r11921a27 <130157306+r11921a27@users.noreply.github.com>
2023-04-19 03:09:50 +0800

    Update profile.jsx

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -119,7 +119,6 @@ Image
                className="relative block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300       placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                onChange={handleImageChange}
              />
-        僅接受jpn,png格式，請注意你的照片會上傳到imgur，請勿上傳機敏照片
 </div>
 </div>
 <div>
